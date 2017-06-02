4aa7cfeb76e4d7b8cee38d3d3c28d8f2bd6d244b
Myriam ZEKRI <meriamzek@hotmail.com>
2017-06-02 13:38:01 +0200

    Merge branch 'master' of https://github.com/AlexiGhost/SubPay_COO_IHM
    # Conflicts:
    #	bin/visual/customer/003 Inscription.fxml
    #	bin/visual/customer/004 Accueil.fxml
    #	bin/visual/customer/004.1 Accueil (authentifier).fxml
    #	bin/visual/customer/005 Menu.fxml
    #	bin/visual/customer/006 Allergies.fxml
    #	bin/visual/customer/007 Pain.fxml
    #	bin/visual/customer/008 Recettes.fxml
    #	bin/visual/customer/009 Garnitures.fxml
    #	bin/visual/customer/010 Sauces.fxml
    #	bin/visual/customer/011 Boissons (Menu).fxml
    #	bin/visual/customer/012 Payer.fxml
    #	bin/visual/manager/Accueil.fxml
    #	build.fxbuild
    #	src/visual/ControllerClient.java
    #	src/visual/customer/002 Connexion.fxml
    #	src/visual/manager/Accueil.fxml

---
+++ src/visual/ControllerClient.java
@@ -0,0 +1,39 @@
+package visual;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ControllerClient extends Application {
+	
+	public void switchScene() throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                //getClass().getResource("iews/Page.fxml")
+        );
+
+        Parent root = (Parent) loader.load();
+        Scene scene = new Scene(root);
+      //  Main.primaryStage.setScene(scene);
+    }
+    
+	public void start(Stage stage) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+    	
+    	Parent acteur = (Parent) loader.load(getClass().getResource("customer/001 Bonjour.fxml"));
+    	
+    	Scene scene = new Scene(acteur, 1280.0, 720.0);
+    	
+    	stage.setTitle("SUBPAY-Accueil");
+        stage.setScene(scene);
+        stage.show();
+    }
+	
+	/**Launch the client interface*/
+    public static void initialize() {
+    	launch();
+    }
+}
