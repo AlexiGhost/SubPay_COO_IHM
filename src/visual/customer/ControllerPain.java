45153ace94457844f6e81623651a5693a5d64e8f
MYRIAM\Admin <meriamzek@hotmail.com>
2017-06-15 11:54:58 +0200

    page recette ignorée si nouveauté ou promo (myriam)

--- src/visual/customer/ControllerPain.java
+++ src/visual/customer/ControllerPain.java
@@ -7,21 +7,28 @@ import javafx.scene.Group;
 
 public class ControllerPain {
 	
-	public void goToRecettes() throws IOException { //Au lieu de "toAccueil", tu dois mettre to + [InterfaceDeDestination]
-		Group acteur = new Group(); //Pas touche
-		acteur.getChildren().add( //Pas touche
-		FXMLLoader.load(getClass().getResource("008 Recettes.fxml")) //Ici, il faut changer le fichier fxml (la string en fait)
-		); //Pas touche
-		visual.ControllerClient.setScene(acteur, "SUBPAY - Recettes"); //Ici, il faut laisser "SUBPAY" et changer "Accueil" selon l'interface où
-																//tu vas. Ca permet de changer le titre de la fenêtre (et ça marche B)  )
+	public void goToRecettes() throws IOException {
+		if(!ControllerAccueil.getNewPromo()){
+			Group acteur = new Group();
+			acteur.getChildren().add( 
+					FXMLLoader.load(getClass().getResource("008 Recettes.fxml"))
+					);
+			visual.ControllerClient.setScene(acteur, "SUBPAY - Recettes");
+		}
+		else{
+			Group acteur = new Group();
+			acteur.getChildren().add( 
+					FXMLLoader.load(getClass().getResource("009 Garnitures.fxml"))
+					);
+			visual.ControllerClient.setScene(acteur, "SUBPAY - Garnitures");
+		}
 	}
 	
-	public void goToMenu() throws IOException { //Au lieu de "toAccueil", tu dois mettre to + [InterfaceDeDestination]
-		Group acteur = new Group(); //Pas touche
-		acteur.getChildren().add( //Pas touche
-		FXMLLoader.load(getClass().getResource("005 Menu.fxml")) //Ici, il faut changer le fichier fxml (la string en fait)
-		); //Pas touche
-		visual.ControllerClient.setScene(acteur, "SUBPAY - Menu"); //Ici, il faut laisser "SUBPAY" et changer "Accueil" selon l'interface où
-																//tu vas. Ca permet de changer le titre de la fenêtre (et ça marche B)  )
+	public void goToMenu() throws IOException {
+		Group acteur = new Group();
+		acteur.getChildren().add(
+		FXMLLoader.load(getClass().getResource("005 Menu.fxml")) 
+		);
+		visual.ControllerClient.setScene(acteur, "SUBPAY - Menu");
 	}
 }
