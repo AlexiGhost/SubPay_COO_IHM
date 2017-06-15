3501f65b2af2e9d4b1839dd9c34aa8dcc3ac16bc
Nebulord <matthieucabanes04@gmail.com>
2017-06-15 21:24:45 +0200

    ControllerSauces (terminé à priori, manque les photos)

--- src/visual/ControllerClient.java
+++ src/visual/ControllerClient.java
@@ -14,6 +14,7 @@ import model.product.composants.Recipe;
 import visual.customer.ControllerAccueil;
 import visual.customer.ControllerGarnitures;
 import visual.customer.ControllerRecettes;
+import visual.customer.ControllerSauces;
 
 public class ControllerClient extends Application {
 	private static 				Stage 			theStage;
@@ -66,6 +67,8 @@ public class ControllerClient extends Application {
 		
 		//Page Garniture
 		ControllerGarnitures.getGarnishList().addAll(ComponentManagement.getGarnishs());
+		//Page Sauce
+		ControllerSauces.getSauceList().addAll(ComponentManagement.getSauces());
 	}
 }
 
